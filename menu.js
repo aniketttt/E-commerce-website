243a14e21fad2ae2c0319afc3289c1c12d040dd7
Aniket Sharma <54195664+aniketttt@users.noreply.github.com>
2022-09-05 20:57:47 +0530

    some update

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
 
 function myfun(){
 	var menu = document.getElementById("items");
-	if (menu.style.display==="none"){
-		menu.style.display="block";
+	if (menu.style.display == "block"){
+		menu.style.display = "none";
 	} else {
-		menu.style.display="none";
+		menu.style.display = "block";
 	}
 };
 
